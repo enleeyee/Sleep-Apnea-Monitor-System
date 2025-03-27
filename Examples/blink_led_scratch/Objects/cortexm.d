172da49a71c11a50e0b1f246cf37f15086f1ccb0
Enlai Yii <enleeyee@gmail.com>
2025-03-27 00:23:39 -0500

    inital project setup

---
+++ Examples/blink_led_scratch/Objects/cortexm.d
@@ -0,0 +1,2 @@
+./objects/cortexm.o: CortexM.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
---
+++ Examples/blink_led_scratch/Objects/launchpad.d
@@ -0,0 +1,3 @@
+./objects/launchpad.o: LaunchPad.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h ..\inc\LaunchPad.h \
+  ..\inc\tm4c123gh6pm.h ..\inc\CortexM.h
