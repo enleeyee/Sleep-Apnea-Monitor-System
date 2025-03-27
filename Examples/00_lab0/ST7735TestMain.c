46a75b97dbb18bb3b4339dd61e00fa018dbbec82
Enlai Yii <enleeyee@gmail.com>
2025-03-26 23:53:46 -0500

    readding lab0 and lab1 code

---
+++ Examples/00_lab0/ST7735TestMain.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+
+int main (void) {
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	GPIO_PORTF_DIR_R = 0x0E;
+	GPIO_PORTF_DEN_R = 0x0E;
+	
+	while(1) GPIO_PORTF_DATA_R = 0x08; // turn on red LED
+}
