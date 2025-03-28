9d54e6f95d945855c7e76b7d81c5c4ddf88df733
Enlai Yii <enleeyee@gmail.com>
2025-03-27 19:30:15 -0500

    added datasheet dir

--- Examples/00_lab0/ST7735TestMain.c
+++ Examples/00_lab0/ST7735TestMain.c
@@ -1,10 +1,33 @@
 #include <stdint.h>
 #include "../inc/tm4c123gh6pm.h"
 
+
+/*
+OLED Display STEMMA QT Connections
+PB2 - I2C0SCL
+PB3 - I2C0SDA
+
+PD1 - I2C3SDA
+PD0 - I2C3SCL
+
+PE5 - I2C2SDA
+PE4 - I2C2SCL
+*/
+
+void delay(long d) {
+	while(d--);
+}
+
 int main (void) {
 	SYSCTL_RCGCGPIO_R |= 0x20;
 	GPIO_PORTF_DIR_R = 0x0E;
 	GPIO_PORTF_DEN_R = 0x0E;
 	
-	while(1) GPIO_PORTF_DATA_R = 0x02; // turn on red LED
+	while(1) {
+		GPIO_PORTF_DATA_R = 0x02; // turn on red LED
+		delay(1000);
+		GPIO_PORTF_DATA_R = 0x04; // blue LED
+		delay(1000);
+		GPIO_PORTF_DATA_R = 0x08; // green LED
+	}
 }
